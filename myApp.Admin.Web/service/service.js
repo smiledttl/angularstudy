ecbc137f7b1af562d0132e4941448c7c2383c62c
ztllb@hotmial.com <ztllb@hotmial.com>
2017-08-18 17:42:16 +0800

    exam13 edit

--- myApp.Admin.Web/service/service.js
+++ myApp.Admin.Web/service/service.js
@@ -1,8 +1,29 @@
-﻿app.factory('UserInformation', function () {
-    var user = {
-        "name": "angular.js"
+﻿angular.module('myApp.services',[])
+.factory('githubService', function ($http) {
+    
+    var gitHubUrl = "https://api.github.com";
+    var githubUsername = "";
+    var runUserRequest = function( path) {
+        // 从使用JSONP调用Github API的$http服务中返回promise
+        return $http({
+            method: 'JSONP',
+            url: gitHubUrl + '/users/' +
+            githubUsername + '/' +
+            path + '?callback=JSON_CALLBACK'
+        });
     };
-    return user;
+    // 返回带有一个events函数的服务对象
+    return {
+        events: function () { return runUserRequest( 'events'); },
+        setUserName: function (username) { githubUsername = username;}
+    };
 
+})
+.factory('xmlParser', function () {
+    var x2js = new X2JS();
+    return {
+        xml2json: x2js.xml2json,
+        json2xml: x2js.json2xml_str
+    };
 });
 
